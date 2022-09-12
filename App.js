fe74cc4321d092227a77da2d511ebe7a8feba2b5
Sahad <govardhan.n@codingmart.com>
2022-09-12 22:25:42 +0530

    commit

--- App.js
+++ App.js
@@ -5,12 +5,15 @@ import React, { useRef } from 'react'
 
 const data = [
   {
+    id: 1,
     url: "https://getwallpapers.com/wallpaper/full/f/0/a/1276880-top-mobile-wallpaper-1080x1920-macbook.jpg"
   },
   {
+    id: 2,
     url: "https://gmedia.playstation.com/is/image/SIEPDC/playstation-wallpapers-persona-5-royal-mobile-wallpaper-01-ps4-27mar20-en-us?$native$"
   },
   {
+    id: 3,
     url: "https://files.oyebesmartest.com/uploads/large/11611041269njn.jpeg",
   }
 ]
@@ -23,7 +26,7 @@ const imageH = Dimensions.get("screen").height;
 const _items = ({ item }) => (
 
   <>
-    <View style={{
+    <View key={item.id} style={{
       display: "flex", alignItems: "center", justifyContent: "center", width: imageW, shadowColor: "#000000",
       shadowOpacity: 0.8,
       shadowRadius: 2,
@@ -44,14 +47,14 @@ const _items = ({ item }) => (
 const MapComponent = ({ scrollX }) => {
 
 
-  return data.map((k, i) => {
-    const inputRange = [(i - 1) * imageW, i * imageW, (i + 1) * imageW];
+  return data.map((k, index) => {
+    const inputRange = [(index - 1) * imageW, index * imageW, (index + 1) * imageW];
     const opacity = scrollX.interpolate({
       inputRange,
       outputRange: [0, 1, 0]
     })
     return (
-      <Animated.Image key={i} source={{ uri: k.url }} style={[StyleSheet.absoluteFillObject, { opacity }]} blurRadius={30} />
+      <Animated.Image key={index} source={{ uri: k.url }} style={[StyleSheet.absoluteFillObject, { opacity }]} blurRadius={30} />
     )
   })
 
@@ -77,7 +80,7 @@ const App = () => {
         <View style={[StyleSheet.absoluteFillObject]}>
           <MapComponent scrollX={scrollX} />
         </View>
-        <Animated.FlatList showsHorizontalScrollIndicator={false} data={data} keyExtractor={(_, index) => { index.toString() }}
+        <Animated.FlatList showsHorizontalScrollIndicator={false} data={data}
           renderItem={_items} horizontal pagingEnabled={true} bounces={false}
           onScroll={Animated.event(
             [{ nativeEvent: { contentOffset: { x: scrollX } } }],
